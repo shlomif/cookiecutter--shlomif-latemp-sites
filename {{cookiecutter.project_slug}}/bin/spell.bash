15fd63d5943b4119829bfa4c6791c4ae903c1a8f
Shlomi Fish <shlomif@shlomifish.org>
2020-05-18 14:53:26 +0300

    correct a filename path.

--- {{cookiecutter.project_slug}}/bin/spell.bash
+++ {{cookiecutter.project_slug}}/bin/spell.bash
@@ -15,7 +15,7 @@ runspell()
         set -e -x
         gmake
         bin/spell-checker-iface > "$spelling_errors_filename_1"
-        perl bin/extract-spelling-spelling_errors_filename_.pl > "$spelling_errors_filename_2"
+        perl bin/extract-spelling-errors.pl > "$spelling_errors_filename_2"
     )
 }
 
