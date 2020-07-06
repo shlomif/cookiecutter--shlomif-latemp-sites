21b48d02651601307a8da81fc1f406653696efae
Shlomi Fish <shlomif@shlomifish.org>
2020-07-06 12:31:16 +0300

    make docbook4 optional

--- {{cookiecutter.project_slug}}/lib/HTML/Latemp/DocBook/GenMake.pm
+++ {{cookiecutter.project_slug}}/lib/HTML/Latemp/DocBook/GenMake.pm
@@ -11,6 +11,7 @@ use HTML::Latemp::DocBook::DocsList   ();
 use HTML::Latemp::DocBook::EndFormats ();
 
 has [ 'dest_var', 'post_dest_var' ] => ( is => 'ro', required => 1 );
+has [ 'disable_docbook4', ]         => ( is => 'ro', default  => '', );
 
 my $gen_make_fn = "lib/make/docbook/sf-homepage-docbooks-generated.mak";
 
@@ -21,14 +22,20 @@ sub generate
     my $tt        = Template->new( {} );
     my $documents = HTML::Latemp::DocBook::DocsList->new->docs_list;
 
-    my $output = '';
+    my $disable_docbook4 = $self->disable_docbook4;
+    my $output           = '';
     $tt->process(
         "lib/make/docbook/sf-homepage-docbook-gen.tt",
         {
+            ( $disable_docbook4 ? ( docbook_versions => [ 5, ] ) : () ),
             DEST      => $self->dest_var,
             POST_DEST => $self->post_dest_var,
-            docs_4    => [ grep { $_->{db_ver} != 5 } @$documents ],
-            docs_5    => [ grep { $_->{db_ver} == 5 } @$documents ],
+            (
+                $disable_docbook4
+                ? ()
+                : ( docs_4 => [ grep { $_->{db_ver} != 5 } @$documents ], )
+            ),
+            docs_5 => [ grep { $_->{db_ver} == 5 } @$documents ],
             fmts =>
                 scalar( HTML::Latemp::DocBook::EndFormats->new->get_formats ),
             top_header => <<"EOF",
